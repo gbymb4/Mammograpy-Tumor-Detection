96bd6b86fe0614f99572433f329e859dde8537c1
gbymb4 <82245621+gbymb4@users.noreply.github.com>
2022-11-21 15:46:24 +1300

    initial structuring of SSD package

---
+++ models/region_detectors/ssd/ssd_efficientnet_v2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 15:45:00 2022
+
+@author: Gavin
+"""
+
---
+++ models/region_detectors/ssd/ssd_resnet.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 15:42:33 2022
+
+@author: Gavin
+"""
+
