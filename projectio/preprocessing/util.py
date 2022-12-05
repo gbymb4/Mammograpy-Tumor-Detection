1aa09995063058ffb4b5ea2a178dd5e04a97a61b
gbymb4 <82245621+gbymb4@users.noreply.github.com>
2022-12-05 17:50:04 +1300

    ROI bounding boxes computation complete

--- projectio/preprocessing/util.py
+++ projectio/preprocessing/util.py
@@ -5,6 +5,8 @@ Created on Thu Dec  1 21:40:49 2022
 @author: Gavin
 """
 
+import numpy as np
+
 def adapt_transformed_coords(
         rois,
         recenter_tracker=None,
@@ -74,3 +76,57 @@ def adapt_transformed_coords(
             roi_data['coords'] = new_roi_points
             
     return rois
+
+
+
+def compute_bounding_boxes(rois):
+    for roi_data in rois.values():
+        
+        bboxes = []
+        for roi_points in roi_data['coords']:
+            roi_points = np.array(roi_points)
+            
+            h_max, h_min = np.max(roi_points[:, 0]), np.min(roi_points[:, 0])
+            w_max, w_min = np.max(roi_points[:, 1]), np.min(roi_points[:, 1])
+        
+            delta_h = h_max - h_min
+            delta_w = w_max - w_min
+            
+            if delta_h > delta_w:
+                widen = (delta_h - delta_w) / 2
+                
+                x1, x2 = w_min - widen, w_max + widen
+                y1, y2 = h_min, h_max
+                
+            elif delta_h < delta_w:
+                heighten = (delta_w - delta_h) / 2
+                
+                x1, x2 = w_min, w_max
+                y1, y2 = h_min - heighten, h_max + heighten
+                
+            elif delta_h == delta_w:
+                x1, x2 = w_min, w_max
+                y1, y2 = h_min, h_max
+                
+            bboxes.append([x1, y1, x2, y2])
+            
+        roi_data['bboxes'] = bboxes
+        
+    return rois
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
