fixed IoU computation for output masks

optim/metrics/segmenters.py
@@ -5,17 +5,23 @@ Created on Tue Jan 17 15:19:33 2023
 @author: Gavin
 """
 
-import torch.nn.functional as F
+import numpy as np
 
-def batch_mask_ious(real, fake):
-    real = real.squeeze(1)
-    fake = fake.squeeze(1)
+def batch_mask_ious(batch1, batch2):
+    batch1 = batch1.detach().cpu().numpy()
+    batch2 = batch2.detach().cpu().numpy()
     
-    intersection = F.intersection(real, fake)
-    
-    union = F.union(real, fake)
+    """
+    Computes the IoU scores between same-index pairs of binary mask images for two batches.
+    Args:
+        batch1 (torch.Tensor): A Tensor of shape (batch_size, 1, H, W) representing the first batch of binary mask images.
+        batch2 (torch.Tensor): A Tensor of shape (batch_size, 1, H, W) representing the second batch of binary mask images.
+    Returns:
+        scores (np.ndarray): A numpy array of shape (batch_size,) containing the IoU scores between the same-index pairs of binary mask images.
+    """
+    intersection = np.sum(np.logical_and(batch1, batch2), axis=(1,2,3))
+    union = np.sum(np.logical_or(batch1, batch2), axis=(1,2,3))
     
     iou = intersection / union
-    iou = iou.detach().cpu().numpy()
     
     return iou